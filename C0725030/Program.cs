868080280793b06b7f83a2d80da52fab8abbd0a2
WaiChun-Leung <waichun.leung29@gmail.com>
2019-03-07 16:47:43 -0500

    Lab 13

--- C0725030/Program.cs
+++ C0725030/Program.cs
@@ -6,45 +6,44 @@ using System.Threading.Tasks;
 
 namespace C0725030
 {
-    //Student Name: Leung Wai Chun      Student ID: C0725030
-    //Student Name: Prabhjot Kaur       Student ID: C0730227
-    //CSD3354 Section 1
-    //Assignment 2
-    //March 7, 2019
-
-    using System;
-
-    delegate void ExampleDelegate(string xyz);
-
-    class Program
+    public class Program
     {
-        public static void Method1(string xyz)
+        public static void Main(string[] args)
         {
-            Console.WriteLine(xyz + " Method1");
-        }
+            //Student Name: Leung Wai Chun      Student ID: C0725030
+            //Student Name: Prabhjot Kaur       Student ID: C0730227
+            //CSD3354 Section 1
+            //Assignment 2
+            //March 7, 2019
 
-        public static void Method2(string xyz)
-        {
-            Console.WriteLine(xyz + " Method2");
-        }
-
-        public static void Main()
-        {            
-            ExampleDelegate ex1Delegate, ex2Delegate, ex3Delegate, myDelegate;
-
-            ex1Delegate = new ExampleDelegate(Method1);
-            ex2Delegate = new ExampleDelegate(Method2);
-            ex3Delegate = ex1Delegate + ex2Delegate;
-            myDelegate = ex1Delegate - ex2Delegate;
-            ex1Delegate("AAA");
-            ex2Delegate("BBB");
-            ex3Delegate("CCC");
-            myDelegate("DDD");
-            myDelegate = ex3Delegate - ex1Delegate;
-            myDelegate("EEE");
-            myDelegate = ex3Delegate - ex2Delegate;
-            myDelegate("FFF");
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
             Console.ReadLine();
+
+        }
+    }
+
+        public delegate int MyDelegate(out int i);
+
+        public class DelegateExercises
+        {
+            int Method1(out int i)
+            {                
+                System.Console.WriteLine("Method1");
+                i = 10;
+                return 0;
+            }
+
+
+        public void Method3()
+        {
+
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            MyDelegate myDelegate1 = new MyDelegate(Method1);
+            MyDelegate myDelegate2 = myDelegate + myDelegate1;
+            int intValue;
+            myDelegate2(out intValue);
+
         }
     }
 }
