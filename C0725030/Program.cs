0bbe32b2239294f2b2087895586dc9706b0c919e
WaiChun-Leung <waichun.leung29@gmail.com>
2019-03-07 13:55:55 -0500

    Lab 1

--- C0725030/Program.cs
+++ C0725030/Program.cs
@@ -10,6 +10,31 @@ namespace C0725030
     {
         static void Main(string[] args)
         {
+            //Student Name: Leung Wai Chun      Student ID: C0725030
+            //Student Name: Prabhjot Kaur       Student ID: C0730227
+            //CSD3354 Section 1
+            //Assignment 2
+            //March 7, 2019
+
+            DelegateExercises myDelegate1 = new DelegateExercises();
+            myDelegate1.Method2();
+        }
+    }
+
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate();
+
+        void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
         }
     }
 }
