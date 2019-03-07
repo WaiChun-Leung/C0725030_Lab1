29176fb8cf07cf0b9a7ae5b1dec1719f5a733d32
WaiChun-Leung <waichun.leung29@gmail.com>
2019-03-07 16:49:14 -0500

    Lab 14

--- C0725030/Program.cs
+++ C0725030/Program.cs
@@ -28,9 +28,9 @@ namespace C0725030
         public class DelegateExercises
         {
             int Method1(out int i)
-            {                
-                System.Console.WriteLine("Method1");
-                i = 10;
+            {
+                i = 100;
+                System.Console.WriteLine("Method1");                
                 return 0;
             }
 
@@ -39,7 +39,7 @@ namespace C0725030
         {
 
             MyDelegate myDelegate = new MyDelegate(Method1);
-            MyDelegate myDelegate1 = new MyDelegate(Method1);
+            MyDelegate myDelegate1 = null;
             MyDelegate myDelegate2 = myDelegate + myDelegate1;
             int intValue;
             myDelegate2(out intValue);
