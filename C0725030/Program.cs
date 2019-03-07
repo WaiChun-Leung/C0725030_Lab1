65a3c4219c00553921d8de01832c88b40b2d34d7
WaiChun-Leung <waichun.leung29@gmail.com>
2019-03-07 16:55:41 -0500

    Lab 15

--- C0725030/Program.cs
+++ C0725030/Program.cs
@@ -17,21 +17,27 @@ namespace C0725030
             //March 7, 2019
 
             DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method3();
-            Console.ReadLine();
+            try
+            {
+                delegateExercises.Method3();
+                Console.ReadLine();
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine("Exception Occurred.");
+                Console.ReadLine();
+            }
 
         }
     }
 
-        public delegate int MyDelegate(out int i);
+        public delegate void MyDelegate();
 
         public class DelegateExercises
         {
-            int Method1(out int i)
+            void Method1()
             {
-                i = 100;
-                System.Console.WriteLine("Method1");                
-                return 0;
+                throw new System.Exception();
             }
 
 
@@ -39,10 +45,7 @@ namespace C0725030
         {
 
             MyDelegate myDelegate = new MyDelegate(Method1);
-            MyDelegate myDelegate1 = null;
-            MyDelegate myDelegate2 = myDelegate + myDelegate1;
-            int intValue;
-            myDelegate2(out intValue);
+            myDelegate();
 
         }
     }
