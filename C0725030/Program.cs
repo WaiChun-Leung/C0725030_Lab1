d5305e291b7c51389997148f5370e75c4721b21f
WaiChun-Leung <waichun.leung29@gmail.com>
2019-03-07 16:31:02 -0500

    Lab 10

--- C0725030/Program.cs
+++ C0725030/Program.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace C0725030
 {
+    //Student Name: Leung Wai Chun      Student ID: C0725030
+    //Student Name: Prabhjot Kaur       Student ID: C0730227
+    //CSD3354 Section 1
+    //Assignment 2
+    //March 7, 2019
+
     using System;
 
     delegate void ExampleDelegate(string xyz);
@@ -23,14 +29,7 @@ namespace C0725030
         }
 
         public static void Main()
-        {
-            //Student Name: Leung Wai Chun      Student ID: C0725030
-            //Student Name: Prabhjot Kaur       Student ID: C0730227
-            //CSD3354 Section 1
-            //Assignment 2
-            //March 7, 2019
-
-
+        {            
             ExampleDelegate ex1Delegate, ex2Delegate, ex3Delegate, myDelegate;
 
             ex1Delegate = new ExampleDelegate(Method1);
