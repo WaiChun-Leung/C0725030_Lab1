fd76a7a5b86d353721762737f56e519839b971c8
WaiChun-Leung <waichun.leung29@gmail.com>
2019-03-07 16:00:05 -0500

    Lab 9

--- C0725030/Program.cs
+++ C0725030/Program.cs
@@ -16,26 +16,13 @@ namespace C0725030
             //Assignment 2
             //March 7, 2019
 
-            DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method3();
-            Console.ReadLine();
 
         }
     }
 
     public delegate void MyDelegate();
 
-    public class DelegateExercises
-    {
-        void Method1()
-        {
-            System.Console.WriteLine("Method1");
-        }
-                
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
-        }
+    public class DelegateExercises : MyDelegate
+    {        
     }
 }
